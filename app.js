b5f9853858a2366093cd0cfe3c29da24aa1fdbf3
Rami <aboomar.2@osu.edu>
2022-08-14 15:12:45 -0400

    intergrated event listener to allow users img to match slection

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hiiii");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const userImg = document.querySelector(".userSelection");
+const btn = [rock, paper, scissors];
+
+btn.forEach((element) => {
+  element.addEventListener("click", () => {
+    let userWeapon = element.innerHTML;
+    userImg.src = `./images/${userWeapon}.jpeg`;
+  });
+});
